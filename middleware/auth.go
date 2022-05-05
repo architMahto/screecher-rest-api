22166bea8c394d1dbab12405165a5d6f7e505e51
architMahto <architmahto@gmail.com>
2022-05-05 08:27:20 -0400

    refactor: refactored isauthenticated middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,26 +1,37 @@
 package middleware
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/architMahto/screecher-rest-api/app/handlers"
 	"github.com/architMahto/screecher-rest-api/utils"
 )
 
+func GetAuthHeaderVal(res http.ResponseWriter, req *http.Request) (*string, error) {
+	authHeader := req.Header.Get("Authorization")
+
+	if authHeader == "" {
+		authError := utils.NewAuthenticationError("User is not logged in")
+		utils.WriteErrorResponse(res, *authError)
+		return nil, errors.New("user is not logged in")
+	}
+
+	return &authHeader, nil
+}
+
 func IsUserAuthenticated(
 	authHandler handlers.AuthHandler,
 	next http.HandlerFunc,
 ) http.HandlerFunc {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		authHeader := req.Header.Get("Authorization")
+		authHeader, authHeaderEmptyErr := GetAuthHeaderVal(res, req)
 
-		if authHeader == "" {
-			authError := utils.NewAuthenticationError("User is not logged in")
-			utils.WriteErrorResponse(res, *authError)
+		if authHeaderEmptyErr != nil {
 			return
 		}
 
-		tokenNotFoundErr := authHandler.AuthService.VerifyTokenInDb(authHeader)
+		tokenNotFoundErr := authHandler.AuthService.VerifyTokenInDb(*authHeader)
 
 		if tokenNotFoundErr != nil {
 			authError := utils.NewAuthenticationError("User is not logged in")
